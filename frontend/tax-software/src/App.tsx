a747e61a1289295c2b0c4ceb611eea1881793659
robinKim23 <dasolrobinkim@gmail.com>
2023-08-02 04:35:25 -0700

    Start Create Account page

--- frontend/tax-software/src/App.tsx
+++ frontend/tax-software/src/App.tsx
@@ -2,6 +2,8 @@ import '@trussworks/react-uswds/lib/index.css';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignInForm from './components/SignInForm';
+import CreateAccountForm from './components/CreateAccountForm';
+
 
 export default function App() {
 
@@ -12,6 +14,7 @@ export default function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/signin" element={<SignInForm />} />
+                    <Route path="/createaccount" element={<CreateAccountForm />} />
                 </Routes>
             </Router>
         </div>
