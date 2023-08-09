904d7b734f950ae28ce019976be8a49fca54a11e
Tobias <tobiashe17@gmail.com>
2023-08-08 22:40:16 -0400

    CRUD functions
    implemented CRUD functions for users collection
    API built

--- backend/tax-software/src/main/java/com/skillstorm/taxsoftware/TaxSoftwareApplication.java
+++ backend/tax-software/src/main/java/com/skillstorm/taxsoftware/TaxSoftwareApplication.java
@@ -2,8 +2,9 @@ package com.skillstorm.taxsoftware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class TaxSoftwareApplication {
 
 	public static void main(String[] args) {
--- backend/tax-software/src/main/java/com/skillstorm/taxsoftware/repository/UserRepository.java
+++ backend/tax-software/src/main/java/com/skillstorm/taxsoftware/repository/UserRepository.java
@@ -1,10 +1,9 @@
 package com.skillstorm.taxsoftware.repository;
 
+import com.skillstorm.taxsoftware.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-import com.skillstorm.taxsoftware.model.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-  User findByOauthId(String oauthId);
 }
