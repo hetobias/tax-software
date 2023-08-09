bf2294c3400151c3720b9e3cf308c8f93904abe0
Tobias <tobiashe17@gmail.com>
2023-08-08 22:40:16 -0400

    CRUD functions
    implemented CRUD functions for users collection
    API built

--- backend/tax-software/src/main/java/com/skillstorm/taxsoftware/controller/UserController.java
+++ backend/tax-software/src/main/java/com/skillstorm/taxsoftware/controller/UserController.java
@@ -1,21 +1,47 @@
 package com.skillstorm.taxsoftware.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import com.skillstorm.taxsoftware.model.User;
 import com.skillstorm.taxsoftware.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
-  @Autowired
-  private UserService userService;
 
-  @GetMapping("/{oauthId}")
-  public User findByOauthId(@PathVariable String oauthId) {
-    return userService.findByOauthId(oauthId);
+  private final UserService userService;
+
+  @PostMapping
+  public User createUser(@RequestBody User user) {
+    return userService.createUser(user);
   }
 
-  // Additional endpoints as required...
+  @GetMapping
+  public List<User> getAllUsers() {
+    return userService.getAllUsers();
+  }
+
+  @GetMapping("/{id}")
+  public User getUserById(@PathVariable String id) {
+    return userService.getUserById(id).orElse(null);
+  }
+
+  @PutMapping("/{id}")
+  public User updateUser(@PathVariable String id, @RequestBody User user) {
+    if (!id.equals(user.getId())) {
+      System.out.println("User with ID: " + id + " not found!");
+      return null;
+    }
+    System.out.println("Update request received for ID: " + id);
+    user.setId(id);
+    return userService.updateUser(user);
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteUser(@PathVariable String id) {
+    userService.deleteUser(id);
+  }
 }
