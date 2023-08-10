f49d225d2d079df3ab3cb1c2e5af858c50f81489
robinKim23 <dasolrobinkim@gmail.com>
2023-08-09 20:45:58 -0700

    Create Constants

---
+++ frontend/tax-software/src/constants.tsx
@@ -0,0 +1,56 @@
+// Tax Rates
+export const TAX_RATE_10 = 0.10;
+export const TAX_RATE_12 = 0.12;
+export const TAX_RATE_22 = 0.22;
+export const TAX_RATE_24 = 0.24;
+export const TAX_RATE_32 = 0.32;
+export const TAX_RATE_35 = 0.35;
+export const TAX_RATE_37 = 0.37;
+
+export const SOCIAL_SECURITY_MAX_WAGE = 142800.00;
+export const MEDICARE_SURTAX_SINGLE = 200000;
+export const MEDICARE_SURTAX_MARRIED = 250000;
+
+export const SOCIAL_SECURITY_TAX_RATE = 0.062;
+export const MEDICARE_TAX_RATE = 0.0145;
+export const MEDICARE_ADDITIONAL_SURTAX_RATE = 0.009;
+
+// Single Filing bracket
+export const SINGLE_BRACKET_10 = 10275.00;
+export const SINGLE_BRACKET_12 = 41775.00;
+export const SINGLE_BRACKET_22 = 89075.00;
+export const SINGLE_BRACKET_24 = 170050.00;
+export const SINGLE_BRACKET_32 = 215950.00;
+export const SINGLE_BRACKET_35 = 539900.00;
+
+export const singleDeduction = 12950.00;
+
+// Married Separate Filing bracket
+export const MARRIED_SEPARATE_BRACKET_10 = 10275.00;
+export const MARRIED_SEPARATE_BRACKET_12 = 41775.00;
+export const MARRIED_SEPARATE_BRACKET_22 = 89075.00;
+export const MARRIED_SEPARATE_BRACKET_24 = 170050.00;
+export const MARRIED_SEPARATE_BRACKET_32 = 215950.00;
+export const MARRIED_SEPARATE_BRACKET_35 = 323925.00;
+
+export const marriedSeparateDeduction = 12950.00;
+
+// Married Joint Filing bracket
+export const MARRIED_JOINT_BRACKET_10 = 20550.00;
+export const MARRIED_JOINT_BRACKET_12 = 83550.00;
+export const MARRIED_JOINT_BRACKET_22 = 178150.00;
+export const MARRIED_JOINT_BRACKET_24 = 340100.00;
+export const MARRIED_JOINT_BRACKET_32 = 431900.00;
+export const MARRIED_JOINT_BRACKET_35 = 647850.00;
+
+export const marriedJointDeduction = 25900.00;
+
+// Head of Household Filing bracket
+export const HEAD_OF_HOUSEHOLD_BRACKET_10 = 14650.00;
+export const HEAD_OF_HOUSEHOLD_BRACKET_12 = 55900.00;
+export const HEAD_OF_HOUSEHOLD_BRACKET_22 = 89050.00;
+export const HEAD_OF_HOUSEHOLD_BRACKET_24 = 170050.00;
+export const HEAD_OF_HOUSEHOLD_BRACKET_32 = 215950.00;
+export const HEAD_OF_HOUSEHOLD_BRACKET_35 = 539900.00;
+
+export const headOfHouseholdDeduction = 19400.00;
